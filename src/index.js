91b7ae5da8301709a2a6a5ecd78b0be667fe1f40
AndIReyf <andrey.prudius@hotmail.com>
2020-09-28 23:18:31 +0200

    feat: add task typical arrays problems solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array || array.length === 0) return 0
+    const min = array.sort((a, b) => a - b)
+    return min[0]
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length === 0) return 0
+    const max = array.sort((a, b) => a - b).reverse()
+    return max[0]
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) return 0
+    const value = array.reduce((sum, num) => {
+        return sum + num
+    },0)
+    return value / array.length
 }
